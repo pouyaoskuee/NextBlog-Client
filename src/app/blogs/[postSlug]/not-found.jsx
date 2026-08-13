0ed7aac2231ee11d3a1ff892f9908e084f38d721
pouya <pouyakhaleghi82@gmail.com>
2026-08-14 03:14:06 +0330

    feat:create blog 404 page

---
+++ src/app/blogs/[postSlug]/not-found.jsx
@@ -0,0 +1,13 @@
+import {useMoveBack} from "@/hooks/useMoveBack";
+import Link from "next/link";
+
+function NotFound() {
+    return (
+        <div className={'flex flex-col p-4 items-center gap-4 '}>
+            <h3 className={'mx-auto'}>صفحه مورد نظر یافت نشد</h3>
+            <Link href={'/blogs'}><p className={'text-secondary-500'}>بازگشت به وبلاگ؟</p></Link>
+        </div>
+    );
+}
+
+export default NotFound;

179c3db4887881299ba04bba67198e7c33f0e5c6
pouya <pouyakhaleghi82@gmail.com>
2026-08-13 22:11:05 +0330

    style:fix gap

--- src/app/blogs/_components/PostsList.js
+++ src/app/blogs/_components/PostsList.js
@@ -6,7 +6,7 @@ async function PostsList() {
     const {data: {posts}} = await result.json();
 
     return (
-        <div className={'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2'}>
+        <div className={'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'}>
             {posts.map((post) => (<PostCard post={post} key={post._id} />))}
         </div>
     )

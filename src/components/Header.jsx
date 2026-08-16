5c31d2b338d1927d85a5ad1ee0e88e047f73ce6f
pouya <pouyakhaleghi82@gmail.com>
2026-08-16 22:13:36 +0330

    feat:header login configs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+"use client"
 import CustomLink from "@/ui/CustomLink";
+import {useAuth} from "@/context/authContext";
+import {UserIcon} from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 function Header() {
+    const {user , isLoading} =  useAuth()
     return (
         <header className={'flex justify-between shadow-lg shadow-secondary-100 p-4 mb-10'}>
             <ul className={'flex gap-6 container'}>
                 <li><CustomLink href={'/'}>خانه</CustomLink></li>
                 <li><CustomLink href={'/blogs'}>بلاگ ها</CustomLink></li>
             </ul>
-            <div>ورود</div>
+            <div className={`${isLoading && 'blur-sm'}`} >{user? <Link href={'/profile'}><UserIcon className={'size-7'}/></Link> : <Link href={'/signin'}>ورود</Link> }</div>
         </header>
     );
 }

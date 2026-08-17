107ee842b9fee75923cba8fe549e1dcc31894828
pouya <pouyakhaleghi82@gmail.com>
2026-08-17 03:51:36 +0330

    feat:handle authorization by refresh token

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -6,6 +6,21 @@ const app = axios.create({
 })
 
 
+app.interceptors.response.use(response => response , async (err) => {
+    const originalConfig = err.response.config;
+    try {
+        if (err.response.status===401 && !originalConfig._retry) {
+            originalConfig._retry = true;
+            const result = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user/refresh-token` , {withCredentials: true});
+            if (result) return app(originalConfig);
+        }
+    }catch(err) {
+        return Promise.reject(err)
+    }
+    return Promise.reject(err)
+});
+
+
 export const http= {
     get: app.get,
     post: app.post,

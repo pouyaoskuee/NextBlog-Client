f723efeb34855badcd1b001326a938263028aa86
pouya <pouyakhaleghi82@gmail.com>
2026-08-19 22:29:56 +0330

    style:fix bugs

--- src/app/blogs/_components/Avatar.jsx
+++ src/app/blogs/_components/Avatar.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Avatar({name , avatarUrl , width=24, height=24}) {
     return (
         <div className={'flex gap-2 items-center justify-center'}>
-            <Image className={'rounded-full'} src={avatarUrl} alt={``} width={width} height={height} />
+            <Image className={'rounded-full'} src={avatarUrl || "/images/avatar.png"} alt={``} width={width} height={height} />
             <p>{name}</p>
         </div>
     );
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -11,7 +11,7 @@ function Modal({ open, onClose, title, children, description = "" }) {
     createPortal(
       <div
         className="backdrop-blur-sm fixed top-0 left-0
-           w-full h-screen bg-secondary-800 bg-opacity-30 z-50"
+           w-full h-screen bg-secondary-600/30 z-50"
       >
         <div
           ref={ref}
